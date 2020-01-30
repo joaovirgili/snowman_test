ddd49b5f6cf2cac96ac6f6bc0677562b50adfd9f
JP <joao@geekbusinesslab.com.br>
2020-01-30 20:20:48 -0300

    Set up bottom nav bar. Must change icons

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:snowmanlabs/app/shared/constants/colors.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -12,12 +13,47 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Stack(
+        children: <Widget>[
+          // Google maps,
+          // Search bar
+        ],
       ),
-      body: Column(
-        children: <Widget>[],
-      ),
+      bottomNavigationBar: MyBottomNavigationBar(),
+    );
+  }
+}
+
+class MyBottomNavigationBar extends StatefulWidget {
+  const MyBottomNavigationBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+
+  var _currentIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      selectedItemColor: MAIN_BLUE,
+      currentIndex: _currentIndex,
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.star), title: Text("")),
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text("")),
+        BottomNavigationBarItem(icon: Icon(Icons.person), title: Text("")),
+      ],
     );
   }
 }
